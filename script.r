aeeba8f04fb005570a255ba766a3ec0091248de4
Margaret  Sun <MargaretSun@MacBook-Pro-8.local>
2019-04-18 13:59:00 -0400

    Updated R script

--- script.r
+++ script.r
@@ -7,4 +7,17 @@ library(ggplot2)
 library(sf)
 
 
-x <- read_csv("http://justicetechlab.org/wp-content/uploads/2018/05/washington_dc_2006to2017.csv")
+x <- read_csv("http://justicetechlab.org/wp-content/uploads/2018/05/washington_dc_2006to2017.csv",
+              cols(
+                incidentid = col_double(),
+                latitude = col_double(),
+                longitude = col_double(),
+                year = col_double(),
+                month = col_double(),
+                day = col_double(),
+                hour = col_double(),
+                minute = col_double(),
+                second = col_double(),
+                numshots = col_double(),
+                type = col_logical()
+              ))
