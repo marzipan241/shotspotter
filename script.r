8550f27685a5f7d7feeeb753fafe72a5dfebaf00
Ruoqi Zhang <ruoqizhang@college.harvard.edu>
2019-04-18 14:27:58 -0400

    Use the shotspotter data to plot a gganimate graph

--- script.r
+++ script.r
@@ -5,19 +5,36 @@ library(tidyverse)
 library(tidycensus)
 library(ggplot2)
 library(sf)
+library(tigris)
+library(gganimate)
 
 
-x <- read_csv("http://justicetechlab.org/wp-content/uploads/2018/05/washington_dc_2006to2017.csv",
-              cols(
-                incidentid = col_double(),
-                latitude = col_double(),
-                longitude = col_double(),
-                year = col_double(),
-                month = col_double(),
-                day = col_double(),
-                hour = col_double(),
-                minute = col_double(),
-                second = col_double(),
-                numshots = col_double(),
-                type = col_logical()
-              ))
+washington_dc <- read_csv("http://justicetechlab.org/wp-content/uploads/2018/05/washington_dc_2006to2017.csv",
+  col_types = cols(
+    incidentid = col_double(),
+    latitude = col_double(),
+    longitude = col_double(),
+    year = col_double(),
+    month = col_double(),
+    day = col_double(),
+    hour = col_double(),
+    minute = col_double(),
+    second = col_double(),
+    numshots = col_double(),
+    type = col_factor()
+  )
+) %>% 
+  filter(!is.na(type))
+
+
+dc_sf <- get_acs(
+  geography = "tract", variables = "B19013_001",
+  state = "DC", geometry = TRUE
+)
+
+urban_areas(class = "sf")
+
+ggplot() +
+  geom_sf(data = dc_sf) +
+  geom_point(data = washington_dc, aes(x = longitude, y = latitude, alpha = 0.1, color = type)) +
+  transition_states(year, transition_length = 0)
